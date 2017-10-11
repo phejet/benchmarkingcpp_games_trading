8cdb4b388cffe3dc6063565461f89ad4e05c0233
Alexander Radchenko <phejet@gmail.com>
2017-10-12 00:27:02 +1100

    add optimised fizzbuzz for the server

---
+++ fizzbuzz_server/main_new.cc
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <cstdlib> 
+#include <stdint.h>
+#include <string>
+#include <type_traits>
+#include <assert.h>
+#include <chrono>
+#include <ctime>
+#include <inttypes.h>
+#include <vector>
+#include "timing.h"
+#include <fstream>
+#include <unistd.h>
+
+
+namespace Demo {
+
+struct FizzBuzzer
+{
+	__attribute__ ((noinline)) 
+	int ReadInt(const char* input)
+	{
+		return std::atoi(input);
+	}
+	
+	__attribute__ ((noinline)) 
+	const char* MakeFizzBuzz(uint64_t number, const char* input)
+	{
+		const bool isFizz = (number % 3) == 0;
+		const bool isBuzz = (number % 5) == 0;
+		if (isFizz && isBuzz)
+		{
+			return "FizzBuzz";
+		}
+		if (isFizz)
+		{
+			return "Fizz";
+		}
+		if (isBuzz)
+		{
+			return "Buzz";
+		}
+		return input;
+	}
+
+	__attribute__ ((noinline)) 
+	void	SendReply(const char* data)
+	{
+		(void)data;
+	}
+
+	__attribute__ ((noinline)) 
+	void HandleRequest(const char* recvBuf)
+	{
+		TimeCapture timeCapture(mTimingLog);		// <--- START TS
+		uint64_t number = ReadInt(recvBuf);
+		timeCapture.CaptureFinishParsingTSC();		// <--- FINISH PARSING TS
+
+		const char* output = MakeFizzBuzz(number, recvBuf);
+		timeCapture.CaptureFinishProcessingTSC();	// <--- FINISH PROCESSING TS
+
+		SendReply(output);
+		timeCapture.CaptureFinishSendTSC();			// <--- FINISH SENDING TS
+
+		timeCapture.SetRequest(number);
+	}
+
+	std::vector<std::string> PrepareData(int num)
+	{
+		std::vector<std::string> values;
+		values.reserve(num);
+		for (int i = 0; i < num; i++)
+		{
+			char buf[33];
+			sprintf(buf, "%d", i);
+			values.push_back(buf);
+		}
+		return values;
+	}
+
+	void SimulationRun(const std::vector<std::string>& testValues)
+	{
+		for (const auto& val : testValues)
+		{
+			HandleRequest(val.c_str());
+			usleep(100);
+		}
+		mTimingLog.DumpLog();		
+	}
+
+	TimeLog mTimingLog;
+};
+
+}
+
+int main(int argc, char *argv[])
+{
+	Demo::FizzBuzzer fizzBuzzer;
+
+    if (argc == 2)
+    {
+    	std::vector<std::string> packets;
+	 	packets.reserve(10000000);
+
+		std::ifstream infile(argv[1]);
+		if (infile)
+		{
+			std::string number;
+			int delayNs = 0;
+	 		while (infile >> number >> delayNs)
+	 		{
+	 			packets.emplace_back(number);
+	 		}
+	 		infile.close();
+	 	}
+		fizzBuzzer.SimulationRun(packets);
+	}
+	else
+	{
+		std::vector<std::string> testValues = fizzBuzzer.PrepareData(30000);
+		fizzBuzzer.SimulationRun(testValues);
+	}
+
+	return 0;
+}
